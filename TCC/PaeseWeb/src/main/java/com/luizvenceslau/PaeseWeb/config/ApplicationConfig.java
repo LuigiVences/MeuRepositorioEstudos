170ed8260a8c54854e00e67c6a9569d73ee2ba1e
Luiz Fernando Venceslau <venceslau.luizfernando@oulook.com>
2025-08-10 14:24:17 -0300

    Adicionando o Bean para habilitar a auditoria

--- TCC/PaeseWeb/src/main/java/com/luizvenceslau/PaeseWeb/config/ApplicationConfig.java
+++ TCC/PaeseWeb/src/main/java/com/luizvenceslau/PaeseWeb/config/ApplicationConfig.java
@@ -5,12 +5,14 @@ import com.luizvenceslau.PaeseWeb.model.entities.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class ApplicationConfig {
 
     @Bean
-    public AuditorAware<User> auditorProvider (){
+    public AuditorAware<User> auditorAware (){
         return new ApplicationAuditAware();
     }
 
