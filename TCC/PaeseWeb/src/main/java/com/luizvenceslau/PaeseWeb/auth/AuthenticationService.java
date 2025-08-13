157ff11f29a896d4b1dc447e5e1e5e293d8f33f0
Luiz Fernando Venceslau <89751328+LuigiVences@users.noreply.github.com>
2025-08-13 18:52:49 +0000

    Ajuste no método de autenticação

--- TCC/PaeseWeb/src/main/java/com/luizvenceslau/PaeseWeb/auth/AuthenticationService.java
+++ TCC/PaeseWeb/src/main/java/com/luizvenceslau/PaeseWeb/auth/AuthenticationService.java
@@ -7,14 +7,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthenticationService {
 
+    private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
 
 
-    public AuthenticationService(JwtService jwtService) {
+    public AuthenticationService(JwtService jwtService, AuthenticationManeger authenticationManager) {
         this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
     }
 
-    public String authenticate(Authentication authentication){
+    public String authenticate(String email, String senha){
+        var authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, senha)
+        );
         return jwtService.generateToken(authentication);
     }
 }
