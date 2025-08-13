bf1d0fc8f7c8e28840c27cdf3a71504ff57127b6
Luiz Fernando Venceslau <89751328+LuigiVences@users.noreply.github.com>
2025-08-13 19:04:33 +0000

    Ajuste do método

--- TCC/PaeseWeb/src/main/java/com/luizvenceslau/PaeseWeb/security/UserAuthenticatedService.java
+++ TCC/PaeseWeb/src/main/java/com/luizvenceslau/PaeseWeb/security/UserAuthenticatedService.java
@@ -20,12 +20,9 @@ public class UserAuthenticatedService implements UserDetailsService {
     @Override
     public UserAuthenticated loadUserByUsername(String email) throws UsernameNotFoundException {
 
-        User user = userRepository.findByEmail(email);
-        if (user == null) {
-            throw new UsernameNotFoundException("Usuário com o e-mail: " + email + " não foi encontrado.");
-        }
-
-        return new UserAuthenticated(user);
+    return userRepository.findByEmail(email)
+            .map(UserAuthenticated::new)
+            .orElseThrow(() -> new UsernameNotFoundException("O usuário com o email: " + email + " não foi encontrado."));
 
     }
 
