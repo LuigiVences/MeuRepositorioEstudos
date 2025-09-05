41b429a8b72d6faaf164acaab42128c7be4ec689
Luiz Fernando Venceslau <venceslau.luizfernando@oulook.com>
2025-09-05 18:09:02 -0300

    Ajuste do Controller

--- TCC/PaeseWeb/src/main/java/com/luizvenceslau/PaeseWeb/controller/CreateUserController.java
+++ TCC/PaeseWeb/src/main/java/com/luizvenceslau/PaeseWeb/controller/CreateUserController.java
@@ -1,13 +1,46 @@
 package com.luizvenceslau.PaeseWeb.controller;
 
+import com.luizvenceslau.PaeseWeb.service.user.UserService;
+import com.luizvenceslau.PaeseWeb.web.user.CreateUserDto;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CreateUserController {
 
+    private final UserService service;
+
+    public CreateUserController(UserService service) {
+        this.service = service;
+    }
+
+
     @GetMapping("/user")
-    public String createUser(){
+    public String showForm(Model model){
+        model.addAttribute("userDto", new CreateUserDto(null, null));
         return "user/create_user";
+    }
+    @PostMapping("/new")
+    public String createUser(@Valid @ModelAttribute("userDto") CreateUserDto userDto,
+                             BindingResult result, Model model){
+        if (result.hasErrors()){
+            return "/user/create_user";
+        }
+
+        try {
+            service.createUser(userDto);
+            model.addAttribute("sucessMessage", "usuário criado com sucesso!");
+            model.addAttribute("userDto", new CreateUserDto(null, null));
+        } catch (IllegalArgumentException e) {
+            result.rejectValue("email", "error.userDto", e.getMessage());
+            return "/user/create_user";
+        }
+
+        return "/user/create_user";
     }
 }
