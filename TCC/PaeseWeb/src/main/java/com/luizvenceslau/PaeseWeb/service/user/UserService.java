f976947c5525b4322c52c3652923765cdca9efed
Luiz Fernando Venceslau <venceslau.luizfernando@oulook.com>
2025-09-07 20:22:12 -0300

    Ajustando a forma como o ID é gerado

--- TCC/PaeseWeb/src/main/java/com/luizvenceslau/PaeseWeb/service/user/UserService.java
+++ TCC/PaeseWeb/src/main/java/com/luizvenceslau/PaeseWeb/service/user/UserService.java
@@ -3,15 +3,22 @@ package com.luizvenceslau.PaeseWeb.service.user;
 import com.luizvenceslau.PaeseWeb.model.entities.User;
 import com.luizvenceslau.PaeseWeb.repository.UserRepository;
 import com.luizvenceslau.PaeseWeb.web.user.CreateUserDto;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final EmailService emailService;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, EmailService emailService) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.emailService = emailService;
     }
 
     public User createUser(CreateUserDto userDto){
@@ -20,9 +27,13 @@ public class UserService {
         }
 
         User user = new User();
+
+        String tempPassword = TempPasswordGenerator.generateNumericPassword();
         user.setName(userDto.name());
         user.setEmail(userDto.email());
-
-        return userRepository.save(user);
+        user.setPassword(tempPassword, passwordEncoder);
+        userRepository.save(user);
+        emailService.sendEmail(user.getEmail(), tempPassword);
+        return user;
     }
 }
